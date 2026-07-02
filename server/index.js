0acf6f3ea1e4c2dec8619e3da7aaef124d99919f
NhienHong <nhienbernie@gmail.com>
2026-07-01 20:01:46 -0500

    fix: temporarily allow all CORS origins for debugging

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express()
 const PORT = process.env.PORT || 4000
 
 // Middleware
-app.use(cors({ origin: process.env.CLIENT_URL }))
+app.use(cors())
 app.use(express.json())
 
 // Routes
