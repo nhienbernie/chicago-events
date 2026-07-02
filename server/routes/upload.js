7148dfd7dca8b0af3dcf0fd90fa26d7e2d571fac
NhienHong <nhienbernie@gmail.com>
2026-07-01 20:31:38 -0500

    fix: disable S3 checksum for presigned URL browser uploads

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -9,7 +9,9 @@ const s3 = new S3Client({
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-  }
+  },
+  requestChecksumCalculation: 'WHEN_REQUIRED',
+  responseChecksumValidation: 'WHEN_REQUIRED'
 })
 
 // POST /api/upload/presign
