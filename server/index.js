f9814ec249c745844d67275e9da459d2869d2f9d
NhienHong <nhienbernie@gmail.com>
2026-07-01 20:11:33 -0500

    reverted previous fix

--- server/index.js
+++ server/index.js
@@ -11,12 +11,7 @@ const app = express()
 const PORT = process.env.PORT || 4000
 
 // Middleware
-app.use(cors({
-  origin: [
-    'http://localhost:3000',
-    'https://chicago-events-psi.vercel.app'
-  ]
-}))
+app.use(cors())
 app.use(express.json())
 
 // Routes
